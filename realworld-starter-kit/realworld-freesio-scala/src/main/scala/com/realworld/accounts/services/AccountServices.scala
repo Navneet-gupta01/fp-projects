708cbadeac8f56f12adb7dd083c5afbf02842cac
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-02-03 09:33:49 +0400

    realworld-freesio-scala: Changes

--- realworld-starter-kit/realworld-freesio-scala/src/main/scala/com/realworld/accounts/services/AccountServices.scala
+++ realworld-starter-kit/realworld-freesio-scala/src/main/scala/com/realworld/accounts/services/AccountServices.scala
@@ -1,6 +1,7 @@
 package com.realworld.accounts.services
 
 import cats.Monad
+import cats.implicits._
 import com.realworld.accounts.model.AccountEntity
 import com.realworld.accounts.persistence.AccountRepository
 import freestyle.tagless.logging.LoggingM
@@ -15,17 +16,17 @@ trait AccountServices[F[_]] {
 
   val repo : AccountRepository[F]
 
-//  def insert(account: AccountEntity): F[Option[AccountEntity]] =
-//    for {
-//      _ <- L.info(s"Inserting model: $model with username: ${account.username} and email: ${account.email}")
-//      accountInserted <- repo.insert(account)
-//      _ <- L.info(s"Successfully Inserted the record for model $model")
-//    } yield accountInserted
-//
-//  def fetch(id: Option[Long], email: Option[String], username: Option[String]): F[List[AccountEntity]] =
-//    for {
-//      _ <- L.info(s"Fetching account for id: $id , email : $email, and username: $username")
-//      accountsFetched <- repo.getUser(id,username,email)
-//      _ <- L.info(s"Successfully Fetched record for model $model")
-//    } yield accountsFetched
+  def insert(account: AccountEntity): F[Option[AccountEntity]] =
+    for {
+      _ <- L.info(s"Inserting model: $model with username: ${account.username} and email: ${account.email}")
+      accountInserted <- repo.insert(account)
+      _ <- L.info(s"Successfully Inserted the record for model $model")
+    } yield accountInserted
+
+  def fetch(id: Option[Long], email: Option[String], username: Option[String]): F[List[AccountEntity]] =
+    for {
+      _ <- L.info(s"Fetching account for id: $id , email : $email, and username: $username")
+      accountsFetched <- repo.getUser(id,username,email)
+      _ <- L.info(s"Successfully Fetched record for model $model")
+    } yield accountsFetched
 }
